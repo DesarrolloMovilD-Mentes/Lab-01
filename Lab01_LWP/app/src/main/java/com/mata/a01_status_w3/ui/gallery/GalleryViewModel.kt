cca70c809be1863616033ee0d684b19735c67c21
unknown <semataoe@est.utn.ac.cr>
2023-10-08 18:05:40 -0600

    First Try to add Navegation Menu

---
+++ Lab01_LWP/app/src/main/java/com/mata/a01_status_w3/ui/gallery/GalleryViewModel.kt
@@ -0,0 +1,13 @@
+package com.mata.a01_status_w3.ui.gallery
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class GalleryViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is gallery Fragment"
+    }
+    val text: LiveData<String> = _text
+}
